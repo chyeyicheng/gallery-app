da700e0cb47e0b665898ba61f4291e40cc5d95bd
chyeyicheng <34520819+chyeyicheng@users.noreply.github.com>
2024-02-12 14:30:21 +0800

    message

--- src/components/ImgContainer.vue
+++ src/components/ImgContainer.vue
@@ -1,14 +1,11 @@
 <template>
-    <transition-group name="fade" tag="div" class="w-full h-full flex flex-col items-center justify-center space-y-4 p-4">
         <div class="w-full h-full flex flex-col items-center justify-center space-y-4 p-4">
-            <ul class="flex flex-wrap gap-4">
+            <transition-group name="fade" tag="ul" class="flex flex-wrap gap-4">
                 <li v-for="document in documents" :key="document.id" class=" w-32 h-32 overflow-hidden rounded-lg shadow-lg">
-
                     <img :src="document.downloadURL" class="w-32 h-32 object-center object-cover" @click="openModal(document.downloadURL)" />
                 </li>
-            </ul>
+            </transition-group>
         </div>
-    </transition-group>
 </template>
 
 <script setup>
@@ -22,3 +19,15 @@ const openModal = (src) => {
     emit("openModal", src);
 };
 </script>
+
+<style scoped>
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity 0.3s, transform 0.3s;
+    }
+    .fade-enter-from,
+    .fade-leave-to {
+        opacity: 0;
+        transform: translateX(30px);
+    }
+</style>
